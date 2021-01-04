96a38ebe8561fd634031579ce28592e4ea2b43f8
Alexa <alexandraws29@outlook.com>
2021-01-03 18:04:37 -0700

    added friends section to user controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -49,7 +49,40 @@ const UserController = {
         })
         .catch(err => res.status(400).JSON(err));
     },
-    
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: {friends: params.friendId }},
+            { new: true }
+        )
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(404).json({message: 'No user found with this id.'});
+                return;
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+    },
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId }},
+            { new: true }
+        )
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No user found with this id.'});
+                return;
+            }
+            res.json(dbUserData)
+        }) 
+        .catch(err => {
+            res.status(400).json(err)
+        })
+    }
 }
 
 
